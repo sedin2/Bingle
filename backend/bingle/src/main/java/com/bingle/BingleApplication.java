3a5eb6fee9ab9fcb66504401ee6e766807a17f25
sedin2 <sedin@kakao.com>
2023-06-22 22:06:26 +0900

    백엔드 프로젝트 생성

---
+++ backend/bingle/src/main/java/com/bingle/BingleApplication.java
@@ -0,0 +1,13 @@
+package com.bingle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BingleApplication.class, args);
+    }
+
+}
---
+++ backend/bingle/src/test/java/com/bingle/BingleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bingle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BingleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
