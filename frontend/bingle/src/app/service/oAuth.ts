662a6e1a050032357dea496e44b28ff442f51c8a
Jongbum <amjong2@gmail.com>
2023-06-24 17:36:42 +0900

    Added kakaoLogin queryString

---
+++ frontend/bingle/src/app/service/oAuth.ts
@@ -0,0 +1,1 @@
+export const queryString = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URL}`;
