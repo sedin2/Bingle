19c55b1fb16dc6034fcefb2926f582f47ac15e69
Jongbum <amjong2@gmail.com>
2023-06-25 00:49:54 +0900

    request to backend

--- frontend/bingle/src/app/oauth/[...nextauth]/page.tsx
+++ frontend/bingle/src/app/oauth/[...nextauth]/page.tsx
@@ -1,14 +1,23 @@
 'use client';
-import { useSearchParams } from 'next/navigation';
+import { redirect, useSearchParams } from 'next/navigation';
 import { PacmanLoader } from 'react-spinners';
+import useSWR from 'swr';
 
 export default function OAuthPage() {
   const searchParams = useSearchParams();
   const code = searchParams.get('code');
-  /* TODO : request to Backend with "code", and handling response from Backend */
+  const getString = `http://localhost:8080/oauth/callback/kakao?code=${code}`;
+  const { data, error, isLoading } = useSWR(getString, fetch);
+
   return (
-    <div className='flex h-screen items-center justify-center'>
-      <PacmanLoader size={40} />
+    <div>
+      {isLoading && (
+        <div className='flex h-screen items-center justify-center'>
+          <PacmanLoader size={40} />
+        </div>
+      )}
+      {error && <h1>error!</h1>}
+      {data && redirect('/')}
     </div>
   );
 }
