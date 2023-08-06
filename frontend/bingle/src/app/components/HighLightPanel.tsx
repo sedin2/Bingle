65062cb194ce1355f3ac51f41b53da0b0c176029
Jongbum <amjong2@gmail.com>
2023-08-06 14:17:48 +0900

    save highlight video id to localstorage for decreasing api request

--- frontend/bingle/src/app/components/HighLightPanel.tsx
+++ frontend/bingle/src/app/components/HighLightPanel.tsx
@@ -15,17 +15,37 @@ const LCKTeamList: string[] = [
   'OK저축은행',
 ];
 
+const getVideoListFromLocalStorage: any = (queryString: string) => {
+  const videoList = localStorage.getItem(queryString);
+  if (!videoList) {
+    return undefined;
+  }
+  return JSON.parse(videoList);
+};
+
 export default function HighLightPanel() {
   const [selectedTeam, setSelectedTeam] = useState<string>(LCKTeamList[0]);
   const [videoList, setVideoList] = useState<string[]>([]);
-  const queryString = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${selectedTeam}&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}`;
+  const queryString = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=LOL ${selectedTeam} 경기 하이라이트&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}`;
   useEffect(() => {
+    /* TODO : need to change caching strategy */
+    /* revalidate certain period */
+    /* TODO : handling loading, error */
+    const videoList = getVideoListFromLocalStorage(queryString);
+    if (videoList) {
+      console.log('from localstoarge');
+      console.log(videoList);
+      setVideoList(videoList);
+      return;
+    }
     fetch(queryString)
       .then((response: any) => response.json())
       .then((data: any) => {
         console.log(data);
         const items = data.items as any[];
-        setVideoList(items.map((item) => item.id.videoId));
+        const videoList = items.map((item) => item.id.videoId);
+        setVideoList(videoList);
+        localStorage.setItem(queryString, JSON.stringify(videoList));
       });
     /* TODO : check CORS Error and change it to using header */
     // fetcher(queryString, 'GET').then((data: any) => {
