a3fe49e064738e56e2c7d5a1e1f130afb6e1d5e6
Jongbum <amjong2@gmail.com>
2023-08-15 11:03:45 +0900

    rollback TeamList component, Modify TeamSelectPanel component

--- frontend/bingle/src/app/components/TeamList.tsx
+++ frontend/bingle/src/app/components/TeamList.tsx
@@ -1,68 +1,27 @@
 import { List, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import IconButton from '@mui/material/IconButton';
 import Avatar from '@mui/material/Avatar';
-import { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 import useTeams from '../hooks/useTeams';
 import { PacmanLoader } from 'react-spinners';
-import CheckIcon from '@mui/icons-material/Check';
-import ToggleButton from '@mui/material/ToggleButton';
 
 export default function TeamList() {
   const [dense, setDense] = useState(false);
   const [secondary, setSecondary] = useState(true);
   const { teams, error, isLoading } = useTeams();
-  const [selectedTeams, setSelectedTeams] = useState<string[]>([]);
-  const handleSelected = useCallback(
-    (event: React.MouseEvent<HTMLElement>) => {
-      let tempTarget = event.target as any;
-      if (tempTarget.tagName != 'BUTTON') {
-        for (
-          ;
-          tempTarget.tagName != 'BUTTON';
-          tempTarget = tempTarget.parentElement
-        );
-        if (tempTarget.tagName != 'BUTTON') return;
-      }
-      if (selectedTeams.includes(tempTarget.id)) {
-        setSelectedTeams((prev) => {
-          return prev.filter((team) => {
-            if (team !== tempTarget.id) return team;
-          });
-        });
-      } else {
-        setSelectedTeams((prev) => {
-          return [...prev, tempTarget.id];
-        });
-      }
-    },
-    [selectedTeams]
-  );
   return (
     <div>
-      <div>
-        <h1 className='font-bold text-3xl text-center'>
-          구독할 팀을 선택하세요!
-        </h1>
-      </div>
       {isLoading && <PacmanLoader size='120px'></PacmanLoader>}
       {error && 'something error'}
       {teams && (
         <List dense={dense}>
           {teams.map(({ name, colorImageUrl }) => (
             <ListItem
-              key={name}
               secondaryAction={
-                <ToggleButton
-                  id={name}
-                  value='check'
-                  selected={
-                    !!selectedTeams.find((team) => {
-                      if (team === name) return true;
-                    })
-                  }
-                  onChange={handleSelected}
-                >
-                  <CheckIcon />
-                </ToggleButton>
+                <IconButton edge='end' aria-label='Add'>
+                  <AddCircleIcon fontSize='large' />
+                </IconButton>
               }
             >
               <ListItemAvatar>
--- frontend/bingle/src/app/components/TeamSelectPanel.tsx
+++ frontend/bingle/src/app/components/TeamSelectPanel.tsx
@@ -1,5 +1,11 @@
+import { List, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import { useCallback, useState } from 'react';
+import useTeams from '../hooks/useTeams';
+import { PacmanLoader } from 'react-spinners';
+import CheckIcon from '@mui/icons-material/Check';
+import ToggleButton from '@mui/material/ToggleButton';
 import { Button } from '@mui/material';
-import TeamList from './TeamList';
 
 type useButton = {
   useThisButton: Boolean;
@@ -15,10 +21,75 @@ export default function TeamSelectPanel({
   useNextButton,
   useSaveButton,
 }: props) {
+  const [dense, setDense] = useState(false);
+  const [secondary, setSecondary] = useState(true);
+  const { teams, error, isLoading } = useTeams();
+  const [selectedTeams, setSelectedTeams] = useState<string[]>([]);
+  const handleSelected = useCallback(
+    (event: React.MouseEvent<HTMLElement>) => {
+      let tempTarget = event.target as any;
+      if (tempTarget.tagName != 'BUTTON') {
+        for (
+          ;
+          tempTarget.tagName != 'BUTTON';
+          tempTarget = tempTarget.parentElement
+        );
+        if (tempTarget.tagName != 'BUTTON') return;
+      }
+      if (selectedTeams.includes(tempTarget.id)) {
+        setSelectedTeams((prev) => {
+          return prev.filter((team) => {
+            if (team !== tempTarget.id) return team;
+          });
+        });
+      } else {
+        setSelectedTeams((prev) => {
+          return [...prev, tempTarget.id];
+        });
+      }
+    },
+    [selectedTeams]
+  );
   return (
     <div>
       <div>
-        <TeamList></TeamList>
+        <div>
+          <div>
+            <h1 className='font-bold text-3xl text-center'>
+              구독할 팀을 선택하세요!
+            </h1>
+          </div>
+          {isLoading && <PacmanLoader size='120px'></PacmanLoader>}
+          {error && 'something error'}
+          {teams && (
+            <List dense={dense}>
+              {teams.map(({ name, colorImageUrl }) => (
+                <ListItem
+                  key={name}
+                  secondaryAction={
+                    <ToggleButton
+                      id={name}
+                      value='check'
+                      selected={selectedTeams.includes(name)}
+                      onChange={handleSelected}
+                      color='success'
+                    >
+                      <CheckIcon />
+                    </ToggleButton>
+                  }
+                >
+                  <ListItemAvatar>
+                    <Avatar alt='temp' src={colorImageUrl}></Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={name}
+                    secondary={secondary ? 'secondary text' : null}
+                  ></ListItemText>
+                </ListItem>
+              ))}
+            </List>
+          )}
+        </div>
       </div>
       <div>
         {useNextButton.useThisButton && (
